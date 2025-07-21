a001476f5062a61801f461f46f6b083d9468a1b6
Nerfdreik <naga79242@mail.ru>
2025-07-22 01:02:30 +0300

    Add files via upload

---
+++ lesson_1_task_5.py
@@ -0,0 +1,13 @@
+def sumNumbers(num):
+    print(num)
+sumNumbers(8) 
+sumNumbers(8)
+sumNumbers(0)
+sumNumbers(0)
+sumNumbers(5)
+sumNumbers(5)
+sumNumbers(5)
+sumNumbers(3)  
+sumNumbers(5)
+sumNumbers(3)
+sumNumbers(5)
