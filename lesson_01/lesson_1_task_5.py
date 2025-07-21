750711d49853c2de6615511bc3d8a818eb46de30
Пользователь <naga79242@mail.ru>
2025-07-22 01:27:58 +0300

    123

---
+++ lesson_01/lesson_1_task_5.py
@@ -0,0 +1,15 @@
+def sumNumbers(num):
+    print(num)
+
+
+sumNumbers(8)
+sumNumbers(8)
+sumNumbers(0)
+sumNumbers(0)
+sumNumbers(5)
+sumNumbers(5)
+sumNumbers(5)
+sumNumbers(3)
+sumNumbers(5)
+sumNumbers(3)
+sumNumbers(5)
