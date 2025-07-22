01f20a498fdc023492ef0885ec0bd9911f1de472
Nerfdreik <naga79242@mail.ru>
2025-07-22 17:20:18 +0300

    lesson_1_task_5.py

--- lesson_1_task_5.py
+++ lesson_1_task_5.py
@@ -1,13 +1,14 @@
-def sumNumbers(num):
-    print(num)
-sumNumbers(8) 
-sumNumbers(8)
-sumNumbers(0)
-sumNumbers(0)
-sumNumbers(5)
-sumNumbers(5)
-sumNumbers(5)
-sumNumbers(3)  
-sumNumbers(5)
-sumNumbers(3)
-sumNumbers(5)
+def print_num(num):
+    print(num, end='')
+
+
+print_num(8)
+print_num(8)
+print_num(0)
+print_num(5)
+print_num(5)
+print_num(5)
+print_num(3)
+print_num(5)
+print_num(3)
+print_num(5)
