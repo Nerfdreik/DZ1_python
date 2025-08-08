f0fef4310647e48d6b86bb6139e643326cbeb830
Nerfdreik <naga79242@mail.ru>
2025-08-08 21:53:18 +0300

    string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,5 +1,5 @@
 class StringUtils:
-    def capitilize(self, string: str) -> str:
+    def capitalize(self, string: str) -> str:
         return string.capitalize()
     
     def trim(self, string: str) -> str:
@@ -24,4 +24,5 @@ class StringUtils:
         return not string.strip()
     
     def list_to_string(self, lst: list, delimiter: str = ", ") -> str:
+
         return delimiter.join(map(str, lst))
