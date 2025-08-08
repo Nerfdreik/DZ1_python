2edd3ccf835520aeb2c3ee98db0f01963878c733
Nerfdreik <naga79242@mail.ru>
2025-08-08 21:54:57 +0300

    test_string_utils.py

--- test_string_utils.py
+++ test_string_utils.py
@@ -4,12 +4,12 @@ from string_utils import StringUtils
 string_utils = StringUtils()
 
 @pytest.mark.positive
-def test_capitilize_positive():
-    assert string_utils.capitilize("тест") == "Тест"
+def test_capitalize_positive():
+    assert string_utils.capitalize("тест") == "Тест"
 
 @pytest.mark.negative
-def test_capitilize_negative():
-    assert string_utils.capitilize("") == ""
+def test_capitalize_negative():
+    assert string_utils.capitalize("") == ""
 
 @pytest.mark.positive
 def test_trim_positive():
@@ -34,3 +34,11 @@ def test_contains_positive():
 @pytest.mark.negative
 def test_contains_negative():
     assert string_utils.contains("", "a") is False
+
+@pytest.mark.positive
+def test_delele_symbol_positive():
+    assert string_utils.delete_symbol("04 апреля 2023", " ") == "04апреля2023"
+
+@pytest.mark.negative
+def test_delete_symbol_negative():
+    assert string_utils.delete_symbol("", "a") == ""
